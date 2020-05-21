54eabbc983749ecc68e3be75579a8c57c7230626
Faizaan Saifi <faizaan.saifi@tothenew.com>
2020-05-21 06:02:38 +0530

    header set

--- app.js
+++ app.js
@@ -35,13 +35,18 @@ app.get('/', (req, res) => {
         })
       }).catch(e => console.log('Error'))
   // res.send(q).status(200)
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET,POST,PUT,PATCH,DELETE,OPTIONS'
-    );
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 })
+
+app.use(function(req, res, next) {
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.headers(
+    //     'Access-Control-Allow-Methods',
+    //     'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+    // );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/name', (req, res) => {
     let q =[];
